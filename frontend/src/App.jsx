d59d1cc354a3afda967f004df346991c43bfee08
DEEPIKA A <259636153+DeepikaAnandhan2006@users.noreply.github.com>
2026-02-18 19:51:16 +0530

    RegistrationPage and dashboard added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./components/Login";
+import RegisterPage from "./components/RegisterPage"; 
+import Dashboard from "./components/Dashboard"; // Import your new Dashboard component
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/admin-dashboard" element={<Dashboard />} /> 
+    </Routes>
+  );
 }
 
 export default App;
